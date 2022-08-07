0448946181d91cb8ee28460e9443638219b73a69
coderou <13739477489@qq.com>
2022-08-07 16:50:12 +0800

    修改publicpath

--- .umirc.ts
+++ .umirc.ts
@@ -4,7 +4,8 @@ export default defineConfig({
   title: 'Site Name',
   mode: 'site',
   // more config: https://d.umijs.org/config
-  history:{
+  history: {
     type: 'hash'
-  }
+  },
+  publicPath: "coderouNote" // 修改publicPath以适配gh-pages的static路径问题
 });
